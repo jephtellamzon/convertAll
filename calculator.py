e5979c668eb7ab66c4f4de6a0c50b536417bbd6c
Jephte Llamzon <62513262+llamzon@users.noreply.github.com>
2022-03-24 13:04:00 +0800

    add addition to calculator

--- calculator.py
+++ calculator.py
@@ -1,2 +1,29 @@
-def debug():
-    print('calculator.py is working')
+def main():
+    print('Welcome to ConvertAll Calculator')
+    print('Please select the operation to be used:')
+    print(
+        '''
+        1. Addition
+        2. Subtraction
+        3. Multiplication
+        4. Division
+        ''')
+    print('Input your selection here:')
+    operationSelect = str(input())
+
+    if operationSelect == '1' or operationSelect == 'one':
+        addition()
+    else:
+        print('Under development')
+
+def addition():
+    print('Please Enter the 1st Number')
+    firstNum = int(input())
+    print('Please Enter the 2nd Number')
+    secondNum = int(input())
+    addAnswer = firstNum + secondNum
+    print('Solution')
+    print(str(firstNum) + ' + ' + str(secondNum) + ' is equal to:')
+    print(addAnswer)
+
+
--- convertAll.py
+++ convertAll.py
@@ -2,18 +2,11 @@ import calculator
 
 print('Welcome to ConvertALL')
 print('Please select from the following:')
-print(
-    '''
-1. Calculator
-2. Measurement Converter
-3. Currency Converter
-4. Medical Converter
-''')
-print('Input your selection here:')
+
 
 mainSelection = str(input())
 
 if mainSelection == '1' or mainSelection == 'one':
-    calculator.debug()
+    calculator.main()
 else:
     print('Under development')
