17084ba81c1c0a9989ddfe2877f53398a24a2c64
Jephte Llamzon <62513262+llamzon@users.noreply.github.com>
2022-03-24 13:18:57 +0800

    fix message

--- convertAll.py
+++ convertAll.py
@@ -2,7 +2,14 @@ import calculator
 
 print('Welcome to ConvertALL')
 print('Please select from the following:')
-
+print(
+    '''
+1. Calculator
+2. Measurement Converter
+3. Currency Converter
+4. Medical Converter
+''')
+print('Input your selection here:')
 
 mainSelection = str(input())
 
