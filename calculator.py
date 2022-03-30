5bdd08a8429495a1d5be97ea8297f86343535362
Jephte Llamzon <62513262+llamzon@users.noreply.github.com>
2022-03-30 11:59:52 +0800

    add remaining arithmetic operations

--- calculator.py
+++ calculator.py
@@ -13,17 +13,55 @@ def main():
 
     if operationSelect == '1' or operationSelect == 'one':
         addition()
+    elif operationSelect == '2' or operationSelect == 'two':
+        subtraction()
+    elif operationSelect == '3' or operationSelect == 'three':
+        multiplication()
+    elif operationSelect == '4' or operationSelect == 'four':
+        division()
     else:
         print('Under development')
+
 
 def addition():
     print('Please Enter the 1st Number')
     firstNum = int(input())
     print('Please Enter the 2nd Number')
     secondNum = int(input())
-    addAnswer = firstNum + secondNum
+    answer = firstNum + secondNum
     print('Solution')
-    print(str(firstNum) + ' + ' + str(secondNum) + ' is equal to:')
-    print(addAnswer)
+    print(str(firstNum) + ' + ' + str(secondNum) +
+          ' is equal to: ' + str(answer))
 
 
+def subtraction():
+    print('Please Enter the 1st Number')
+    firstNum = int(input())
+    print('Please Enter the 2nd Number')
+    secondNum = int(input())
+    answer = firstNum - secondNum
+    print('Solution')
+    print(str(firstNum) + ' - ' + str(secondNum) +
+          ' is equal to: ' + str(answer))
+
+
+def multiplication():
+    print('Please Enter the 1st Number')
+    firstNum = int(input())
+    print('Please Enter the 2nd Number')
+    secondNum = int(input())
+    answer = firstNum * secondNum
+    print('Solution')
+    print(str(firstNum) + ' x ' + str(secondNum) +
+          ' is equal to: ' + str(answer))
+
+
+def division():
+    print('Please Enter the 1st Number')
+    firstNum = int(input())
+    print('Please Enter the 2nd Number')
+    secondNum = int(input())
+    answer = firstNum / secondNum
+    print('Solution')
+    print(str(firstNum) + ' / ' + str(secondNum) +
+          ' is equal to: ' + str(answer))
